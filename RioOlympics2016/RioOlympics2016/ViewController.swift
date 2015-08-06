bd32c5ebf9de79c474545a11b37d8626719d0846
秦绪海 <465648485@qq.com>
2015-08-06 14:35:49 +0800

    commit1

---
+++ RioOlympics2016/RioOlympics2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RioOlympics2016
+//
+//  Created by 秦绪海 on 15/8/5.
+//  Copyright (c) 2015年 qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
