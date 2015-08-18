2293c429c1346057dc6852d9707879e8d73e89fb
秦绪海 <465648485@qq.com>
2015-08-18 22:18:54 +0800

    commit

---
+++ RioOlympics2016/RioOlympics2016/EventsDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventsDetailViewController.swift
+//  RioOlympics2016
+//
+//  Created by 秦绪海 on 15/8/17.
+//  Copyright (c) 2015年 qin. All rights reserved.
+//
+
+import UIKit
+
+class EventsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RioOlympics2016/RioOlympics2016/EventsViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventsViewCell.swift
+//  RioOlympics2016
+//
+//  Created by 秦绪海 on 15/8/17.
+//  Copyright (c) 2015年 qin. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewCell: UICollectionViewCell {
+    
+}
